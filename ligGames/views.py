5b9c35de73ee089c286756ff41ab16893d4197cf
sonali-97 <sonalirai103@gmail.com>
2017-12-03 04:33:09 +0530

    Fix home page error

--- ligGames/views.py
+++ ligGames/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 #from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from ligGames.forms import SignUpForm
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
 
 def signup(request):
     if request.method == 'POST':
